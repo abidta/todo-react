043b9c9e4842bb805e1bfd8da3b643c2d3a14eed
Sainul Abid <sainulabidofficial@gmail.com>
2023-10-19 16:29:25 +0530

    Adding todos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
   return (
     <div className="app">
-    <div className="mainHeading">
-      <h1>ToDo List</h1>
-    </div>
-    <div className="subHeading">
-      <br />
-      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-    </div>
-    <div className="input">
-      <input type="text" placeholder="🖊️ Add item..." />
-      <i className="fas fa-plus"></i>
-    </div>
-    <div className="todos">
-      <div className="todo">
-        <div className="left">
-          <input type="checkbox" name="" id="" />
-          <p>Rect tutorial</p>
-        </div>
-        <div className="right">
-          <i className="fas fa-times"></i>
-        </div>
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
       </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => {
+            setToDo(e.target.value);
+          }}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={(e) => {
+            setToDos([
+              ...toDos,
+              { title: toDo, id: Date.now(), status: false },
+            ]);
+            setToDo("");
+          }}
+          className="fas fa-plus"
+        ></i>
+      </div>
+      {toDos.map((todoObj) => {
+        return (
+          <div className="todos">
+            <div className="todo">
+              <div className="left">
+                <input type="checkbox" name="" id="" />
+                <p>{todoObj.title}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  </div>
   );
 }
 
